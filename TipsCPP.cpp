9db4faa27ce413c169be2b69362995632ffaf525
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-09-19 12:18:53 -0400

    Update TipsCPP.cpp

--- TipsCPP.cpp
+++ TipsCPP.cpp
@@ -300,4 +300,236 @@ public:
 };
 
 
-//
+//9. Naive implementation of Trie
+class TrieNode{
+public:
+    bool isEnd;
+    vector<TrieNode*> branches;
+    TrieNode(bool end = false): isEnd(end){
+        branches = vector<TrieNode*>(26, nullptr);
+    }
+};
+
+class Trie {
+private:
+    TrieNode* root;
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+        root = new TrieNode(false);
+    }
+    
+    ~Trie(){
+        destroy(root);
+    }
+    void destroy(TrieNode* node){
+        for(auto c : node->branches){
+            if(c != nullptr)
+                destroy(c);
+        }
+        delete node;
+    }
+    
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        int len = word.size();
+        TrieNode* node = root;
+        int i = 0;
+        for(; i < len; i++){
+            if(!node->branches[word[i]-'a']){
+                break;
+            }
+            else{
+                node = node->branches[word[i] - 'a'];
+                //A very important step which guarantee that when we finishes insertion, 
+                //We set the termination to be true
+                node->isEnd = (i == len-1)? true : node->isEnd;
+            }
+        }
+        for(; i<len; i++){
+            node->branches[word[i] - 'a'] = new TrieNode(i == len-1? true:false);
+            node = node->branches[word[i] - 'a'];
+        }
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        TrieNode* node = root;
+        int len = word.size();
+        for(int i = 0; i < len; i++){
+            if(!node->branches[word[i] - 'a'])
+                return false;
+            else{
+                node = node->branches[word[i] - 'a'];
+            }
+        }
+        return node->isEnd;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+        int len = prefix.size();
+        for(int i = 0; i < len; i++){
+            if(!node->branches[prefix[i] - 'a'])
+                return false;
+            else{
+                node = node->branches[prefix[i] - 'a'];
+            }
+        }
+        return true;
+    }
+};
+
+
+
+//10. Random number generator
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+/* initialize random seed: */
+srand (time(NULL));
+
+/* generate secret number between 1 and 10: */
+iSecret = rand() % 10 + 1;
+
+
+//11. Union Find Data structure to solve cycle detection in graph
+// A union by rank and path compression based program to detect cycle in a graph 
+#include <stdio.h> 
+#include <stdlib.h> 
+
+// a structure to represent an edge in the graph 
+struct Edge 
+{ 
+	int src, dest; 
+}; 
+
+// a structure to represent a graph 
+struct Graph 
+{ 
+	// V-> Number of vertices, E-> Number of edges 
+	int V, E; 
+
+	// graph is represented as an array of edges 
+	struct Edge* edge; 
+}; 
+
+struct subset 
+{ 
+	int parent; 
+	int rank; 
+}; 
+
+// Creates a graph with V vertices and E edges 
+struct Graph* createGraph(int V, int E) 
+{ 
+	struct Graph* graph = (struct Graph*) malloc( sizeof(struct Graph) ); 
+	graph->V = V; 
+	graph->E = E; 
+
+	graph->edge = (struct Edge*) malloc( graph->E * sizeof( struct Edge ) ); 
+
+	return graph; 
+} 
+
+// A utility function to find set of an element i 
+// (uses path compression technique) 
+int find(struct subset subsets[], int i) 
+{ 
+	// find root and make root as parent of i (path compression) 
+	if (subsets[i].parent != i) 
+		subsets[i].parent = find(subsets, subsets[i].parent); 
+
+	return subsets[i].parent; 
+} 
+
+// A function that does union of two sets of x and y 
+// (uses union by rank) 
+void Union(struct subset subsets[], int x, int y) 
+{ 
+	int xroot = find(subsets, x); 
+	int yroot = find(subsets, y); 
+
+	// Attach smaller rank tree under root of high rank tree 
+	// (Union by Rank) 
+	if (subsets[xroot].rank < subsets[yroot].rank) 
+		subsets[xroot].parent = yroot; 
+	else if (subsets[xroot].rank > subsets[yroot].rank) 
+		subsets[yroot].parent = xroot; 
+
+	// If ranks are same, then make one as root and increment 
+	// its rank by one 
+	else
+	{ 
+		subsets[yroot].parent = xroot; 
+		subsets[xroot].rank++; 
+	} 
+} 
+
+// The main function to check whether a given graph contains cycle or not 
+int isCycle( struct Graph* graph ) 
+{ 
+	int V = graph->V; 
+	int E = graph->E; 
+
+	// Allocate memory for creating V sets 
+	struct subset *subsets = 
+		(struct subset*) malloc( V * sizeof(struct subset) ); 
+
+	for (int v = 0; v < V; ++v) 
+	{ 
+		subsets[v].parent = v; 
+		subsets[v].rank = 0; 
+	} 
+
+	// Iterate through all edges of graph, find sets of both 
+	// vertices of every edge, if sets are same, then there is 
+	// cycle in graph. 
+	for(int e = 0; e < E; ++e) 
+	{ 
+		int x = find(subsets, graph->edge[e].src); 
+		int y = find(subsets, graph->edge[e].dest); 
+
+		if (x == y) 
+			return 1; 
+
+		Union(subsets, x, y); 
+	} 
+	return 0; 
+} 
+
+// Driver program to test above functions 
+int main() 
+{ 
+	/* Let us create the following graph 
+		0 
+		| \ 
+		| \ 
+		1-----2 */
+
+	int V = 3, E = 3; 
+	struct Graph* graph = createGraph(V, E); 
+
+	// add edge 0-1 
+	graph->edge[0].src = 0; 
+	graph->edge[0].dest = 1; 
+
+	// add edge 1-2 
+	graph->edge[1].src = 1; 
+	graph->edge[1].dest = 2; 
+
+	// add edge 0-2 
+	graph->edge[2].src = 0; 
+	graph->edge[2].dest = 2; 
+
+	if (isCycle(graph)) 
+		printf( "Graph contains cycle" ); 
+	else
+		printf( "Graph doesn't contain cycle" ); 
+
+	return 0; 
+} 
+
+
