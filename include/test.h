7120e7254083fb25f021009831671eeeee0eccca
Jun Zhang <zhangxiaomu01@gmail.com>
2023-07-13 22:08:33 +0800

    Modifies the template CPP project to use Cmake build system.

---
+++ include/test.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include<iostream>
+
+void printUs() {
+    std::cout << "I like apple!" << std::endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include<windows.h>
 #include<iostream>
 
+#include<test.h>
+
 int main() {
     std::cout << "Good one!" << std::endl;
+    printUs();
 }
